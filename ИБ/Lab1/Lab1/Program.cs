b84b65715684be8eb704b267110612d815a836b9
grebiar <nik17092001@mail.ru>
2023-02-17 16:24:33 +0300

    lab 1 - 2)

--- ИБ/Lab1/Lab1/Program.cs
+++ ИБ/Lab1/Lab1/Program.cs
@@ -6,18 +6,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        TestEncryption(new PolybeanSquare());
-    }
+        TestEncryption.Test(new PolybeanSquare());
 
-    static void TestEncryption(IEncryption encryption)
-    {
-        string text = "тест? ещё и много";
-        Console.WriteLine("Тестирование шифрования - "+encryption.Name);
-
-        Console.WriteLine("Текст который будем шифровать - " + text);
-        string en = encryption.Encrypt(text);
-        Console.WriteLine("Зашифрованный текст - " + en);
-        string de = encryption.Decrypt(en);
-        Console.WriteLine("Расшифрованный текст - " + de);
+        TestEncryption.Test(new ReshuffleByKey("лунатик"));
     }
 }
---
+++ ИБ/Lab1/Lab1/TestEncryption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal static class TestEncryption
+    {
+        public static void Test(IEncryption encryption)
+        {
+            string text = "НЕЯСНОЕ СТАНОВИТСЯ ЕЩЕ БОЛЕЕ НЕПОНЯТНЫМ";
+            Console.WriteLine("Тестирование шифрования - " + encryption.Name);
+
+            Console.WriteLine("Текст который будем шифровать - " + text);
+            string en = encryption.Encrypt(text);
+            Console.WriteLine("Зашифрованный текст - " + en);
+            string de = encryption.Decrypt(en);
+            Console.WriteLine("Расшифрованный текст - " + de);
+        }
+    }
+}
